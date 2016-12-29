cd1b2a1faad82e7cc269f1835363816165ade5ab
wang_wonderful <wang_wonderful@foxmail.com>
2016-12-29 10:59:34 +0800

    NSArray,NSMutabeArray注释

--- Pods/YYCategories/YYCategories/Foundation/NSArray+YYAdd.h
+++ Pods/YYCategories/YYCategories/Foundation/NSArray+YYAdd.h
@@ -20,7 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  Creates and returns an array from a specified property list data.
- 
+ 从plist 数据转成数组
  @param plist   A property list data whose root object is an array.
  @return A new array created from the plist data, or nil if an error occurs.
  */
@@ -28,7 +28,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  Creates and returns an array from a specified property list xml string.
- 
+ 从plist 字符串转成数组
  @param plist   A property list xml string whose root object is an array.
  @return A new array created from the plist string, or nil if an error occurs.
  */
@@ -36,21 +36,21 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  Serialize the array to a binary property list data.
- 
+ 数组序列化成plist 数据
  @return A bplist data, or nil if an error occurs.
  */
 - (nullable NSData *)plistData;
 
 /**
  Serialize the array to a xml property list string.
- 
+ 数组序列化成plist 字符串
  @return A plist xml string, or nil if an error occurs.
  */
 - (nullable NSString *)plistString;
 
 /**
  Returns the object located at a random index.
- 
+ 返回数组中的随机为止的对象
  @return The object in the array with a random index value.
  If the array is empty, returns nil.
  */
@@ -59,7 +59,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Returns the object located at index, or return nil when out of bounds.
  It's similar to `objectAtIndex:`, but it never throw exception.
- 
+ 安全的获取数组里的对象，如果index超出count，则返回nil
  @param index The object located at index.
  */
 - (nullable id)objectOrNilAtIndex:(NSUInteger)index;
@@ -67,6 +67,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Convert object to json string. return nil if an error occurs.
  NSString/NSNumber/NSDictionary/NSArray
+ 把string，number，字典，数组转成json字符串，其它对象则返回nil
  */
 - (nullable NSString *)jsonStringEncoded;
 
@@ -85,7 +86,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  Creates and returns an array from a specified property list data.
- 
+ 从plist数据生成可变数组
  @param plist   A property list data whose root object is an array.
  @return A new array created from the plist data, or nil if an error occurs.
  */
@@ -93,7 +94,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  Creates and returns an array from a specified property list xml string.
- 
+ 从plist字符串生成可变数组
  @param plist   A property list xml string whose root object is an array.
  @return A new array created from the plist string, or nil if an error occurs.
  */
@@ -102,7 +103,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Removes the object with the lowest-valued index in the array.
  If the array is empty, this method has no effect.
- 
+ 移除数组里的第一个数据
  @discussion Apple has implemented this method, but did not make it public.
  Override for safe.
  */
@@ -111,7 +112,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Removes the object with the highest-valued index in the array.
  If the array is empty, this method has no effect.
- 
+ 移除数组里最后一个数据
  @discussion Apple's implementation said it raises an NSRangeException if the
  array is empty, but in fact nothing will happen. Override for safe.
  */
@@ -120,7 +121,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Removes and returns the object with the lowest-valued index in the array.
  If the array is empty, it just returns nil.
- 
+ 移除第一个数据，并返回
  @return The first object, or nil.
  */
 - (nullable id)popFirstObject;
@@ -128,14 +129,14 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Removes and returns the object with the highest-valued index in the array.
  If the array is empty, it just returns nil.
- 
+ 移除最后数据，并返回
  @return The first object, or nil.
  */
 - (nullable id)popLastObject;
 
 /**
  Inserts a given object at the end of the array.
- 
+ 在最后插入数据
  @param anObject The object to add to the end of the array's content.
  This value must not be nil. Raises an NSInvalidArgumentException if anObject is nil.
  */
@@ -143,7 +144,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  Inserts a given object at the beginning of the array.
- 
+ 在最前面插入数据
  @param anObject The object to add to the end of the array's content.
  This value must not be nil. Raises an NSInvalidArgumentException if anObject is nil.
  */
@@ -152,7 +153,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Adds the objects contained in another given array to the end of the receiving
  array's content.
- 
+ 在最后插入数组
  @param objects An array of objects to add to the end of the receiving array's
  content. If the objects is empty or nil, this method has no effect.
  */
@@ -161,7 +162,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Adds the objects contained in another given array to the beginnin of the receiving
  array's content.
- 
+ 在最前面插入数组
  @param objects An array of objects to add to the beginning of the receiving array's
  content. If the objects is empty or nil, this method has no effect.
  */
@@ -170,7 +171,7 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Adds the objects contained in another given array at the index of the receiving
  array's content.
- 
+ 在某个位置插入数据
  @param objects An array of objects to add to the receiving array's
  content. If the objects is empty or nil, this method has no effect.
  
@@ -183,11 +184,13 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  Reverse the index of object in this array.
  Example: Before @[ @1, @2, @3 ], After @[ @3, @2, @1 ].
+ 翻转数组
  */
 - (void)reverse;
 
 /**
  Sort the object in this array randomly.
+ 打乱数组
  */
 - (void)shuffle;
 
